4d262d4d1f67b636c355b56db22748e3a36034cf
Brandi Lauria <n01057930@unf.edu>
2020-04-07 09:31:20 -0400

    Add project files.

---
+++ project2blauria/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace project2blauria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpPlayerTwo = new System.Windows.Forms.GroupBox();
+            this.btnEnterLetter = new System.Windows.Forms.Button();
+            this.lblGuessesRemaining = new System.Windows.Forms.Label();
+            this.txtGuessesRemaining = new System.Windows.Forms.TextBox();
+            this.lblWordToGuess = new System.Windows.Forms.Label();
+            this.txtWordToGuess = new System.Windows.Forms.TextBox();
+            this.lstYouGuessed = new System.Windows.Forms.ListBox();
+            this.grpPlayerOne = new System.Windows.Forms.GroupBox();
+            this.cboNumberOfGuesses = new System.Windows.Forms.ComboBox();
+            this.btnEnterWordToGuess = new System.Windows.Forms.Button();
+            this.txtEnterLetter = new System.Windows.Forms.TextBox();
+            this.txtEnterWordToGuess = new System.Windows.Forms.TextBox();
+            this.grpPlayerTwo.SuspendLayout();
+            this.grpPlayerOne.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpPlayerTwo
+            // 
+            this.grpPlayerTwo.Controls.Add(this.txtEnterLetter);
+            this.grpPlayerTwo.Controls.Add(this.btnEnterLetter);
+            this.grpPlayerTwo.Controls.Add(this.lblGuessesRemaining);
+            this.grpPlayerTwo.Controls.Add(this.txtGuessesRemaining);
+            this.grpPlayerTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpPlayerTwo.Location = new System.Drawing.Point(13, 13);
+            this.grpPlayerTwo.Name = "grpPlayerTwo";
+            this.grpPlayerTwo.Size = new System.Drawing.Size(452, 130);
+            this.grpPlayerTwo.TabIndex = 0;
+            this.grpPlayerTwo.TabStop = false;
+            this.grpPlayerTwo.Text = "Player 2";
+            // 
+            // btnEnterLetter
+            // 
+            this.btnEnterLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnterLetter.Location = new System.Drawing.Point(44, 80);
+            this.btnEnterLetter.Name = "btnEnterLetter";
+            this.btnEnterLetter.Size = new System.Drawing.Size(117, 33);
+            this.btnEnterLetter.TabIndex = 3;
+            this.btnEnterLetter.Text = "Enter Letter";
+            this.btnEnterLetter.UseVisualStyleBackColor = true;
+            this.btnEnterLetter.Click += new System.EventHandler(this.btnEnterLetter_Click);
+            // 
+            // lblGuessesRemaining
+            // 
+            this.lblGuessesRemaining.AutoSize = true;
+            this.lblGuessesRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessesRemaining.Location = new System.Drawing.Point(240, 80);
+            this.lblGuessesRemaining.Name = "lblGuessesRemaining";
+            this.lblGuessesRemaining.Size = new System.Drawing.Size(153, 20);
+            this.lblGuessesRemaining.TabIndex = 2;
+            this.lblGuessesRemaining.Text = "Guesses Remaining";
+            // 
+            // txtGuessesRemaining
+            // 
+            this.txtGuessesRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuessesRemaining.Location = new System.Drawing.Point(290, 36);
+            this.txtGuessesRemaining.Name = "txtGuessesRemaining";
+            this.txtGuessesRemaining.Size = new System.Drawing.Size(50, 26);
+            this.txtGuessesRemaining.TabIndex = 0;
+            // 
+            // lblWordToGuess
+            // 
+            this.lblWordToGuess.AutoSize = true;
+            this.lblWordToGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWordToGuess.Location = new System.Drawing.Point(153, 146);
+            this.lblWordToGuess.Name = "lblWordToGuess";
+            this.lblWordToGuess.Size = new System.Drawing.Size(140, 24);
+            this.lblWordToGuess.TabIndex = 1;
+            this.lblWordToGuess.Text = "Word to Guess!";
+            // 
+            // txtWordToGuess
+            // 
+            this.txtWordToGuess.Location = new System.Drawing.Point(57, 173);
+            this.txtWordToGuess.Name = "txtWordToGuess";
+            this.txtWordToGuess.Size = new System.Drawing.Size(335, 20);
+            this.txtWordToGuess.TabIndex = 2;
+            // 
+            // lstYouGuessed
+            // 
+            this.lstYouGuessed.FormattingEnabled = true;
+            this.lstYouGuessed.Location = new System.Drawing.Point(57, 212);
+            this.lstYouGuessed.Name = "lstYouGuessed";
+            this.lstYouGuessed.Size = new System.Drawing.Size(335, 121);
+            this.lstYouGuessed.TabIndex = 3;
+            // 
+            // grpPlayerOne
+            // 
+            this.grpPlayerOne.Controls.Add(this.txtEnterWordToGuess);
+            this.grpPlayerOne.Controls.Add(this.cboNumberOfGuesses);
+            this.grpPlayerOne.Controls.Add(this.btnEnterWordToGuess);
+            this.grpPlayerOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpPlayerOne.Location = new System.Drawing.Point(13, 351);
+            this.grpPlayerOne.Name = "grpPlayerOne";
+            this.grpPlayerOne.Size = new System.Drawing.Size(452, 122);
+            this.grpPlayerOne.TabIndex = 4;
+            this.grpPlayerOne.TabStop = false;
+            this.grpPlayerOne.Text = "Player 1";
+            // 
+            // cboNumberOfGuesses
+            // 
+            this.cboNumberOfGuesses.AutoCompleteCustomSource.AddRange(new string[] {
+            "3",
+            "5",
+            "10",
+            "15"});
+            this.cboNumberOfGuesses.FormattingEnabled = true;
+            this.cboNumberOfGuesses.Items.AddRange(new object[] {
+            "3",
+            "5",
+            "10",
+            "15"});
+            this.cboNumberOfGuesses.Location = new System.Drawing.Point(265, 37);
+            this.cboNumberOfGuesses.Name = "cboNumberOfGuesses";
+            this.cboNumberOfGuesses.Size = new System.Drawing.Size(168, 28);
+            this.cboNumberOfGuesses.TabIndex = 2;
+            this.cboNumberOfGuesses.Text = "Select # of Guesses";
+            this.cboNumberOfGuesses.SelectedIndexChanged += new System.EventHandler(this.cboNumberOfGuesses_SelectedIndexChanged);
+            // 
+            // btnEnterWordToGuess
+            // 
+            this.btnEnterWordToGuess.Location = new System.Drawing.Point(44, 73);
+            this.btnEnterWordToGuess.Name = "btnEnterWordToGuess";
+            this.btnEnterWordToGuess.Size = new System.Drawing.Size(184, 38);
+            this.btnEnterWordToGuess.TabIndex = 0;
+            this.btnEnterWordToGuess.Text = "Enter Word To Guess";
+            this.btnEnterWordToGuess.UseVisualStyleBackColor = true;
+            this.btnEnterWordToGuess.Click += new System.EventHandler(this.btnEnterWordToGuess_Click);
+            // 
+            // txtEnterLetter
+            // 
+            this.txtEnterLetter.Location = new System.Drawing.Point(44, 36);
+            this.txtEnterLetter.Name = "txtEnterLetter";
+            this.txtEnterLetter.Size = new System.Drawing.Size(117, 26);
+            this.txtEnterLetter.TabIndex = 4;
+            // 
+            // txtEnterWordToGuess
+            // 
+            this.txtEnterWordToGuess.Location = new System.Drawing.Point(44, 26);
+            this.txtEnterWordToGuess.Name = "txtEnterWordToGuess";
+            this.txtEnterWordToGuess.Size = new System.Drawing.Size(184, 26);
+            this.txtEnterWordToGuess.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 485);
+            this.Controls.Add(this.grpPlayerOne);
+            this.Controls.Add(this.lstYouGuessed);
+            this.Controls.Add(this.txtWordToGuess);
+            this.Controls.Add(this.lblWordToGuess);
+            this.Controls.Add(this.grpPlayerTwo);
+            this.Name = "Form1";
+            this.Text = "Guess the Word!";
+            this.grpPlayerTwo.ResumeLayout(false);
+            this.grpPlayerTwo.PerformLayout();
+            this.grpPlayerOne.ResumeLayout(false);
+            this.grpPlayerOne.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpPlayerTwo;
+        private System.Windows.Forms.Button btnEnterLetter;
+        private System.Windows.Forms.Label lblGuessesRemaining;
+        private System.Windows.Forms.TextBox txtGuessesRemaining;
+        private System.Windows.Forms.Label lblWordToGuess;
+        private System.Windows.Forms.TextBox txtWordToGuess;
+        private System.Windows.Forms.ListBox lstYouGuessed;
+        private System.Windows.Forms.GroupBox grpPlayerOne;
+        private System.Windows.Forms.ComboBox cboNumberOfGuesses;
+        private System.Windows.Forms.Button btnEnterWordToGuess;
+        private System.Windows.Forms.TextBox txtEnterLetter;
+        private System.Windows.Forms.TextBox txtEnterWordToGuess;
+    }
+}
+
---
+++ project2blauria/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Brandi Lauria
+//COP 2010 - Project 2
+//
+//Player 1 Inputs Word to guess and number of guesses given to player 2 clears the field of the word player 1 entered that player 2 must guess.
+//Puts equals signs in the amount per the length of the Word player 1 entered so player 2 can see the length of the word and displays 
+//the amount of incorrect guess allowed by Player 2.
+//allows Player 1 to enter the letter they would like to guess but only allows one character to go through the program.
+//if two characters entered error message appears.
+//if player 2 enters the correct letter replace letter in position in the textbox displaying equal signs characters for word entered by Player 1 
+//and display message saying you have chosen the correct letter in the listbox.
+//If player enters the wrong letter output in listbox wrong letter chosen and countdown the number of guesses
+//When the Word has been unhidden meaning changed from equals signs to complete word output message box you won.
+//When guess countdown reaches 0 output error message telling the player that they have lost.
+//
+//March 7, 2017
+namespace project2blauria
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Variables Declared for Switch
+        int intNumberOfGuesses;
+        int intGuessesRemaining;
+        //Variables Declared for Word to be guessed
+
+
+        private void cboNumberOfGuesses_SelectedIndexChanged(object sender, EventArgs e)
+        {//begin method
+
+            //Number of Guesses to integers
+            intNumberOfGuesses = int.Parse(cboNumberOfGuesses.Text);
+
+            //Switch statement for Combobox to pull Guess allowed
+            switch (intNumberOfGuesses)
+            {//begin switch statement for cbo box
+                case 3:
+                    intGuessesRemaining = 3;
+                    break;
+                case 5:
+                    intGuessesRemaining = 5;
+                    break;
+                case 10:
+                    intGuessesRemaining = 10;
+                    break;
+                case 15:
+                    intGuessesRemaining = 15;
+                    break;
+            }//end switch statement for cbo box
+
+            //Display result
+           txtGuessesRemaining.Text = intGuessesRemaining.ToString();
+
+        }//end method
+        //field variables
+        string strEnterWord = "";
+        string strnewDash = "";
+        private void btnEnterWordToGuess_Click(object sender, EventArgs e)
+        {//begin method
+
+
+            //Get input data from textbox
+            strEnterWord = (txtEnterWordToGuess.Text);
+            strEnterWord.ToString();
+            //retrieve the length of the word
+            strEnterWord.Substring(strEnterWord.Length);
+
+            //put in = sign for string length
+            strnewDash = new string('=', strEnterWord.Length);
+            // display = in length of word entered
+            (txtWordToGuess.Text) = strnewDash;
+            //Clear text so Player 2 can't see
+            txtEnterWordToGuess.Clear();
+
+        }//end method
+
+
+        private void btnEnterLetter_Click(object sender, EventArgs e)
+        {//begin method
+
+            //local variables
+            string strLetterEntered = "";
+            string strWord;
+            int position;
+            bool blnGameOver;
+            //get text from box text box
+            strLetterEntered = (txtEnterLetter.Text);
+            strWord = (strEnterWord);
+
+            
+
+
+
+            //if text is not one character loop- error message
+            if ((txtEnterLetter.Text.Length == 0 || (txtEnterLetter.Text.Length >= 2)))
+            {// if decision begins if letter longer than one character display an error message
+                MessageBox.Show("Enter one character only please try again");
+                return;
+            }//if decision statement end
+
+
+            //Local Variables
+            int intGuessCountDown;
+            intGuessCountDown = int.Parse(txtGuessesRemaining.Text);
+            if (!strWord.Contains(strLetterEntered) && intGuessCountDown > 0)
+
+            { // if letter entered is not contained in word
+                //listbox message letter wasn't found please guess again
+                lstYouGuessed.Items.Add(strLetterEntered + " wasn't found - please guess again");
+                //remaining guesses count--
+                intGuessCountDown--;
+                //Display countdown
+                txtGuessesRemaining.Text = intGuessCountDown.ToString();
+            }//if decision statement end
+
+            if (intGuessCountDown == 0)
+            {//if decision statement begins if - guess countdown reaches zero 
+            //game over you lost
+                MessageBox.Show("Game Over - You Lost");
+          
+                blnGameOver = true;
+                return;
+            }//if decision statement end
+
+
+
+            //if letter guessed is in mysteryword
+            if (strWord.Contains(strLetterEntered) && txtEnterWordToGuess.Text != strWord)
+
+            {// begin decision of if statement if the letter is contained in the word and is not unhidden then
+                //tell them they guessed correctly and make a new string that recieves the letter entered in hidden word
+                // and output correct guess
+                lstYouGuessed.Items.Add(" You guessed the letter " + strLetterEntered + " correctly!");
+
+                StringBuilder sb = new StringBuilder(txtWordToGuess.Text);
+                position = strEnterWord.IndexOf(strLetterEntered);
+                sb.Remove(position, 1);
+                sb.Insert(position, strLetterEntered);
+                txtWordToGuess.Text = sb.ToString();
+
+            }//end decision of if statement
+
+            //listbox message you guessed the letter "" correctly!
+            if (txtWordToGuess.Text == strWord)
+            {//begin decision of if statement if Word to guess is uncovered they won
+                MessageBox.Show("You Won - Congratulations");
+                return;
+            }//end decision of if statement
+
+        }//end method
+
+
+    }//end class
+}//namespace
